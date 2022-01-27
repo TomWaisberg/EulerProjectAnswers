e84617e5632f61b2d160b3efb26a13a3362893c3
Tom Waisberg <68729153+TomWaisberg@users.noreply.github.com>
2022-01-27 19:36:56 +0200

    Delete problem3.py

--- problem3.py
+++
@@ -1,8 +0,0 @@
-# https://projecteuler.net/problem=3
-
-num = 600851475143
-factors = [71, 839, 1471, 6857, 59569, 104441, 486847, 1234169, 5753023, 10086647, 87625999, 408464633]
-for i in range(408464634, num):
-    print(i)
-    if(num % i == 0):
-        factors.append(i)
